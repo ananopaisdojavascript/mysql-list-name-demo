6c9d33785957e1e110037aafa760ee2a7067f6e5
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-04-24 15:10:02 -0300

    save

--- index.ts
+++ index.ts
@@ -11,7 +11,8 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectionLimit: 10
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));

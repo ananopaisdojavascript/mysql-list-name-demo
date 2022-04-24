4ce32f002bec83a31e3b3df120982b74214d9c13
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-04-24 15:17:02 -0300

    .

--- index.ts
+++ index.ts
@@ -7,12 +7,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10
+    database: process.env.DB_NAME
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
